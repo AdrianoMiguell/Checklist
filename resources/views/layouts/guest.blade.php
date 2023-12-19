7c426da5324e117273eafbb0a68c5f791fc4d794
AdrianoMiguell <adrianosantos260804@gmail.com>
2023-12-19 08:00:01 -0300

    Ajustes nas áreas de login e register

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -25,19 +25,18 @@
         <link rel="stylesheet" href="./css/welcome.css">
     @endif
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans text-gray-900" style="background-color: rgb(var(--dark-c));">
     @if (isset($slot))
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+        <div class="d-flex flex-column justify-content-center align-items-center py-5">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="w-25 h-25" />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg "
+            <div class="w-50 m-5 px-5 py-4 shadow-md overflow-hidden rounded-3 "
                 style="background-color: rgba(var(--quin-c), .75); color: rgb(var(--light-c))">
                 {{ $slot }}
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,72 +3,13 @@
 @include('layouts.navigation')
 
 @section('welcome')
-    <style>
-
-        .sectionInit {
-            padding: 1rem 2rem;
-            display: flex;
-            flex-grow: 1;
-            gap: 2rem;
-            flex-direction: column;
-            justify-content: space-around;
-        }
-
-        .divMessage {
-            min-height: 250px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-wrap: wrap;
-            gap: .5rem;
-        }
-
-        .messageImg,
-        .messageText {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-grow: 1;
-            gap: 1rem;
-            height: 100%;
-        }
-
-        .messageImg {
-            padding: 0 1rem;
-        }
-
-        .messageImg img {
-            width: 300px;
-            height: auto;
-        }
-
-        .messageText {
-            width: 300px;
-            padding: 0 2rem;
-            color: rgb(var(--light-c));
-            text-shadow: .5px .5px .5px rgba(var(--sec-c), .75);
-            flex-direction: column;
-            text-align: center;
-        }
-
-        .messageText h1 {
-            font-size: 20pt;
-            font-weight: 650;
-        }
-
-        .messageText .fraseMotivacional {
-            font-size: 12pt;
-            font-weight: 450;
-        }
-    </style>
-
-    <div class="sectionInit">
+    <section class="container">
 
         <div class="divMessage">
 
             <div class="messageImg" class="changeImages">
                 <a href="https://br.freepik.com/vetores-gratis/pessoas-ilustradas-desenhadas-a-mao-que-planejam-negocios_19948931.htm#query=organiza%C3%A7%C3%A3o&position=10&from_view=search&track=sph"
-                    class="referenceImg" target="_blank" ><img src="/img/organizando_o_tempo_com_checklist.svg"
+                    class="referenceImg" target="_blank"><img src="/img/organizando_o_tempo_com_checklist.svg"
                         alt="imagem sobre organização, retirada do Freepik" /></a>
             </div>
             <div class="messageText">
@@ -76,15 +17,15 @@
                 <div class="fraseMotivacional">
                     " Com organização e tempo, acha-se o segredo de fazer tudo e bem feito. " - Pitágoras
                 </div>
+                <div class="inviteClick">
+                    <h2 class="text-center fs-3">Venha organizar o seu dia! </h2>
+                    <a href="{{ url('dashboard') }}" class="btnG btnG-light-green" style="padding: 10px 20px;"> Clique para
+                        começar </a>
+                </div>
             </div>
 
         </div>
 
-        <div class="inviteClick d-flex flex-column justify-content-center align-items-center">
-            <h2 class="text-center">Venha organizar o seu dia! </h2>
-            <a href="{{ url('dashboard') }}" class="btnG btnG-light-green" style="padding: 10px 20px;"> Clique para
-                começar </a>
-        </div>
 
-    </div>
+    </section>
 @endsection
